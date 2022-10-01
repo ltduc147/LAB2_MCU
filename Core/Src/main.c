3184dac7395c8f4620a4afb1f80052595d989f1c
Duc <ltduc147@gmail.com>
2022-10-01 16:07:59 +0700

    Ex3

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -225,48 +225,8 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
-const int MAX_LED = 4;
-int index_led = 0;
-int led_buffer[4] = {0, 3, 7, 9};
-void update7SEG(int index){
-	int number_display;
-	switch (index) {
-		case 0:
-			//Display the first 7SEG with led_buffer[0]
-			HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 0);
-			HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 1);
-			HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, 1);
-			HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, 1);
-			number_display = led_buffer[0];
-			break;
-		case 1:
-			//Display the second 7SEG with led_buffer[1]
-			HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 1);
-			HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 0);
-			HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, 1);
-			HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, 1);
-			number_display = led_buffer[1];
-			break;
-		case 2:
-			//Display the third 7SEG with led_buffer[2]
-			HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 1);
-			HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 1);
-			HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, 0);
-			HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, 1);
-			number_display = led_buffer[2];
-			break;
-		case 3:
-			//Display the forth 7SEG with led_buffer[3]
-			HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 1);
-			HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 1);
-			HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, 1);
-			HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, 0);
-			number_display = led_buffer[3];
-			break;
-		default:
-			break;
-	}
-	switch (number_display){
+void display7SEG(int num){
+	switch (num){
 		case 0:
 			GPIOB->ODR =0x0040;
 			break;
@@ -296,6 +256,48 @@ void update7SEG(int index){
 			break;
 		case 9:
 			GPIOB->ODR =0x0010;
+			break;
+		default:
+			break;
+	}
+}
+
+const int MAX_LED = 4;
+int index_led = 0;
+int led_buffer[4] = {4, 5, 8, 9};
+void update7SEG(int index){
+	switch (index) {
+		case 0:
+			//Display the first 7SEG with led_buffer[0]
+			HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 1);
+			HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, 1);
+			HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, 1);
+			display7SEG(led_buffer[0]);
+			HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 0);
+			break;
+		case 1:
+			//Display the second 7SEG with led_buffer[1]
+			HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 1);
+			HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, 1);
+			HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, 1);
+			display7SEG(led_buffer[1]);
+			HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 0);
+			break;
+		case 2:
+			//Display the third 7SEG with led_buffer[2]
+			HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 1);
+			HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 1);
+			HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, 1);
+			display7SEG(led_buffer[2]);
+			HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, 0);
+			break;
+		case 3:
+			//Display the forth 7SEG with led_buffer[3]
+			HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 1);
+			HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 1);
+			HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, 1);
+			display7SEG(led_buffer[3]);
+			HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, 0);
 			break;
 		default:
 			break;
